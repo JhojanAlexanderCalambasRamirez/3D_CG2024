f4843c1dd1d75aa31f36e0f00873ee5a8a5e8785
JACR <98819854+JhojanAlexanderCalambasRamirez@users.noreply.github.com>
2024-11-07 09:07:14 -0500

    Cambio de escena y arreglo de bugs
    Unicamente se modifico em cambio de script de cambio de escena para que cambie dependiendo de la cantidad de colisiones y bugs arreglados con los canvas duplicados en al escena bosque.

--- CG_2024Juego/Assets/SCRIPTS/MecanicaJuego/cambioEscena.cs
+++ CG_2024Juego/Assets/SCRIPTS/MecanicaJuego/cambioEscena.cs
@@ -1,25 +1,48 @@
-//Alexander Calambas - 2190555
-//Juan Manuel Santos - 2215928
-//Juan David Rios - 2225674
-
+// Alexander Calambas - 2190555
+// Juan Manuel Santos - 2215928
+// Juan David Rios - 2225674
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CambioEscena : MonoBehaviour
 {
+    private static int contadorColisiones = 0; // Variable estática para que el contador se mantenga entre escenas
+
     private void OnTriggerEnter(Collider other)
     {
-        // Verifica si el objeto que colisiona tiene el tag "Player1"
         if (other.CompareTag("Player1"))
         {
-            // Cambia a la escena con índice 0
-            SceneManager.LoadScene(0);
-        }
-        else if (other.CompareTag("Player2"))
-        {
-            // Cambia a la escena con índice 1
-            SceneManager.LoadScene(1);
+            contadorColisiones++; // Incrementa el contador en cada colisión
+
+            // Cambia de escena según el valor del contador
+            switch (contadorColisiones)
+            {
+                case 1: // Primera colisión, carga la escena Mina
+                    SceneManager.LoadScene(1);
+                    break;
+                case 2: // Segunda colisión, carga la escena MenuPrincipal
+                    SceneManager.LoadScene(0);
+                    break;
+                case 3: // Tercera colisión, carga la escena DemoRed
+                    SceneManager.LoadScene(4);
+                    break;
+                case 4: // Cuarta colisión, carga la escena Bosque
+                    SceneManager.LoadScene(2);
+                    break;
+                case 5: // Quinta colisión, carga la escena DemoGreen
+                    SceneManager.LoadScene(5);
+                    break;
+                case 6: // Sexta colisión, carga la escena Bosque
+                    SceneManager.LoadScene(2);
+                    break;
+                case 7: // Séptima colisión, carga la escena DemoBlue
+                    SceneManager.LoadScene(6);
+                    break;
+                case 8: // Octava colisión, carga la escena Creditos/Final
+                    SceneManager.LoadScene(7);
+                    break;
+            }
         }
     }
 }
